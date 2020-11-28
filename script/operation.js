7405ffc4f9342f097243f17756c1303dce5e9566
CLF <1529760075@qq.com>
2020-11-28 14:05:01 +0800

    the new repo. old one move here

---
+++ script/operation.js
@@ -0,0 +1,43 @@
+/** 登入系统 **/
+
+function loginConfirm(){
+    return;
+}
+
+/** 退出系统 **/
+function logoutConfirm(){
+    if(confirm("您确定要退出本系统吗？")){
+        window.location.href = "login.html";
+    }
+}
+
+
+/** 数据传送部分 **/
+
+function queryForDish(){
+    function requestForDish(){
+        
+    }
+    return;
+}
+
+function addDish(){
+    return;
+}
+
+function modifyDish(){
+    return;
+}
+
+
+function deleteDish(){
+    return;
+}
+
+function queryForUser(){
+    return;
+}
+
+function addUser(){
+    return;
+}
